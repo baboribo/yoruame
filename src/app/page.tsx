2175870d76815759bcf1da7a2676a51dafcaace0
baboribo <inertia_mailer_0c@icloud.com>
2025-10-06 17:46:19 +0900

    style: 두 폰트 스타일 맞추기.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
       <header className="pt-20">
         <h2>홈</h2>
       </header>
-      <section className="mt-3 gap-1 flex flex-col">
-        <h4>人権の無視及び軽侮が、人類の良心を踏みにじった野蛮行為をもたらし、言論及び信仰の自由が受けられ、恐怖及び欠乏のない世界の到来が</h4>
-        <h5>인권의 무시 및 경멸이 인류의 양심을 짓밟는 야만적인 행위들을 초래했으며, 언론과 신앙의 자유를 누리고 공포와 결핍이 없는 세상이 도래하기를</h5>
-        <p>That recognition of the inherent dignity and of the equal and inalienable rights of all members of the human family is the foundation of freedom, justice and peace in the world.</p>
-        <p className='mt-3'>그냥 폰트 사이즈 테스트.</p>
+      <section className="mt-3 gap-1 flex flex-col">        
+        <h3>"인권의 무시 및 경멸이 인류의 양심을 짓밟는 야만적인 행위들을 초래했으며,<br /> 언론과 신앙의 자유를 누리고 공포와 결핍이 없는 세상이 도래하기를"</ㅗ>
+        <h3>人権の無視及び軽侮が、人類の良心を踏みにじった野蛮行為をもたらし、<br />言論及び信仰の自由が受けられ、恐怖及び欠乏のない世界の到来が</h3>
+        <p>"That recognition of the inherent dignity and of the equal and inalienable rights of all members of the human family is the foundation of freedom, justice and peace in the world."</p>
+        <p className='mt-3'>- 세계인권선언 전문(前文)의 일부</p>
       </section>
     </div>
   );
